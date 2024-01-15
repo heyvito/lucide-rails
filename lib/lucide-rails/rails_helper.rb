0a374b7de83d6ab1266a0dab9e303b73e0751479
Mathias Maisberger <me@hiasinho.com>
2024-01-15 22:02:16 +0000

    add class concatenation for helper

--- lib/lucide-rails/rails_helper.rb
+++ lib/lucide-rails/rails_helper.rb
@@ -3,6 +3,13 @@
 module LucideRails
   module RailsHelper
     def lucide_icon(named, **options)
+      if options[:class]
+        options[:class] = [
+          LucideRails.default_options[:class] || "",
+          options[:class]
+        ].join(" ")
+      end
+
       content_tag(:svg,
                   IconProvider.icon(named).html_safe,
                   LucideRails.default_options.merge(**options))
